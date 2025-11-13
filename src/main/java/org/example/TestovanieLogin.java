7805d339c76a66d609484e46f7c355c94b9788a9
Milos Valcak <valcak.milos@gmail.com>
2025-11-13 21:53:40 +0100

    commit -m "Priadnie ku kazdej dielcej ulohe, aby som vedel co zbehlo: System.out.println("Otvorenie stránky" atd.)"

---
+++ src/main/java/org/example/TestovanieLogin.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class TestovanieLogin {
+private static WebDriver driver;
+
+  @BeforeAll
+  public static void setUp() {
+    System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Desktop\\ProgramovanieKody\\SkillmeaOOP2\\SkilmeaBDD\\SkillmeaSelenium\\SkillmeaSelenium\\src\\main\\resources\\chromedriver.exe");
+    driver = new ChromeDriver();
+  }
+
+  @Test
+  public void testLogin() {
+    System.out.println("Otvorenie stránky");
+    driver.get("https://www.tutorialspoint.com/selenium/practice/login.php");
+    System.out.println("Vyplnenie hesla");
+    driver.findElement(By.id("password")).sendKeys("heslo");
+    System.out.println("Vyplnenie emailu");
+    driver.findElement(By.id("email")).sendKeys("novyemail@email.com");
+    System.out.println("Kliknutie na Login");
+    driver.findElement(By.xpath("//input[@value='Login']")).click();
+    System.out.println("Overenie výsledku");
+    assertEquals("Welcome, Login In", driver.findElement(By.xpath("//*[@id=\"signInForm\"]/h1")).getText());
+
+
+
+  }
+}
